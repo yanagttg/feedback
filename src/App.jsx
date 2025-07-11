9d65b7ab636bed64ff444a56d7ea3fff7e89f9ad
Yana GttG <y.gottselig@gmail.com>
2025-07-11 12:25:40 +0200

    po4inila ves code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import { FeedbackForm } from "./components/FeedbackForm/FeedbackForm";
 import { Modal } from "./components/Modal/Modal";
 
 function App() {
+  const [form, setForm] = useState({}); 
+  const [showModal, setShowModal] = useState(false);
+
+  const onSubmit = (data) => {
+    setForm(data);
+    setShowModal(true);
+  };
+  
   return (
     <>
-      <FeedbackForm />
-      <Modal show>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque sed,
-        minima cum aspernatur voluptate tenetur, dicta provident minus debitis
-        rem esse enim. Tempore, doloremque veniam unde iste ipsa ea distinctio.
-      </Modal>
+      <FeedbackForm onSubmit={onSubmit} />
+      {form && (
+      <Modal title="Спасибо, что пишете" show={showModal}>
+        {form.username}, Благодарим за заявку! <br/>
+      Мы отправим ответ на Вашу почту {form.email}
+       </Modal>
+       )}
     </>
   );
 }
